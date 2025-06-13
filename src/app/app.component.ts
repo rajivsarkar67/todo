7fa1d4ef2705d086a6fb160b6837aa08a2ce737e
@Rajibkumar.Sarkar <rajibkumar.sarkar@bounteous.com>
2025-06-13 23:44:33 +0530

    feat: updated angular to version 19

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,11 +4,10 @@ import { CommonModule } from '@angular/common';
 import { SubTodo, Todo } from './custom.interface';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet, CommonModule],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    imports: [RouterOutlet, CommonModule],
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css'
 })
 export class AppComponent {
 
